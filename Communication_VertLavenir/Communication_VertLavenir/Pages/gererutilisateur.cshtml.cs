1ee00dc1adbc8c7b963df52cb32baf0382e56101
Raybay <2388204@cegepmontpetit.ca>
2026-05-05 10:38:26 -0400

    ajout page gererutilisateur

---
+++ Communication_VertLavenir/Communication_VertLavenir/Pages/gererutilisateur.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Communication_VertLavenir.Pages
+{
+    public class gererutilisateurModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
